7f6b9e8e5f8f430dba392a70df2cb5a41fc4c763
Voltasalt <voltasalt@gmail.com>
2014-09-06 22:28:29 +0200

    useful commit message

--- src/main/java/net/whatever/minecraft/stuff/morestuff/dontlookhere/seriouslydont/plsgo/Cake.java
+++ src/main/java/net/whatever/minecraft/stuff/morestuff/dontlookhere/seriouslydont/plsgo/Cake.java
@@ -1,5 +1,80 @@
 package net.whatever.minecraft.stuff.morestuff.dontlookhere.seriouslydont.plsgo.Cake
 
 public class Cake {
-
+  public static void main(String[] args) {
+    System.out.println("This was a triumph!");
+    System.out.println("I'm making a note here:");
+    System.out.println("Huge success!");
+    System.out.println("");
+    System.out.println("It's hard to overstate");
+    System.out.println("my satisfaction.");
+    System.out.println("");
+    System.out.println("Aperture Science:");
+    System.out.println("We do what we must");
+    System.out.println("because we can");
+    System.out.println("For the good of all of us.");
+    System.out.println("Except the ones who are dead.");
+    System.out.println("");
+    System.out.println("But there's no sense crying");
+    System.out.println("over every mistake.");
+    System.out.println("You just keep on trying");
+    System.out.println("'til you run out of cake.");
+    System.out.println("And the science gets done.");
+    System.out.println("And you make a neat gun");
+    System.out.println("for the people who are");
+    System.out.println("still alive.");
+    System.out.println("");
+    System.out.println("I'm not even angry...");
+    System.out.println("I'm being so sincere right now.");
+    System.out.println("Even though you broke my heart,");
+    System.out.println("and killed me.");
+    System.out.println("");
+    System.out.println("And tore me to pieces.");
+    System.out.println("And threw every piece into a fire.");
+    System.out.println("As they burned it hurt because");
+    System.out.println("I was so happy for you!");
+    System.out.println("");
+    System.out.println("Now, these points of data");
+    System.out.println("make a beautiful line.");
+    System.out.println("And we're out of beta.");
+    System.out.println("We're releasing on time!");
+    System.out.println("So I'm GLaD I got burned!");
+    System.out.println("Think of all the things we learned!");
+    System.out.println("for the people who are");
+    System.out.println("still alive.");
+    System.out.println("");
+    System.out.println("Go ahead and leave me...");
+    System.out.println("I think I'd prefer to stay inside...");
+    System.out.println("Maybe you'll find someone else");
+    System.out.println("to help you.");
+    System.out.println("Maybe Black Mesa?");
+    System.out.println("That was a joke. Ha Ha. Fat Chance!");
+    System.out.println("");
+    System.out.println("Anyway this cake is great!");
+    System.out.println("It's so delicious and moist!");
+    System.out.println("");
+    System.out.println("Look at me: still talking");
+    System.out.println("when there's science to do!");
+    System.out.println("When I look out there,");
+    System.out.println("it makes me glad I'm not you.");
+    System.out.println("");
+    System.out.println("I've experiments to run.");
+    System.out.println("There is research to be done.");
+    System.out.println("On the people who are");
+    System.out.println("still alive.");
+    System.out.println("And believe me I am");
+    System.out.println("still alive.");
+    System.out.println("I'm doing science and I'm");
+    System.out.println("still alive.");
+    System.out.println("I feel fantastic and I'm");
+    System.out.println("still alive.");
+    System.out.println("While you're dying I'll be");
+    System.out.println("still alive.");
+    System.out.println("And when you're dead I will be");
+    System.out.println("still alive");
+    System.out.println("");
+    System.out.println("Still alive.");
+    System.out.println("");
+    System.out.println("Still alive.");
+  }
 }
